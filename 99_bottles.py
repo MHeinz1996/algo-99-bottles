cee9c0e48e0b256545e3923dab8367347b0f366d
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-23 15:23:58 -0500

    Completed 99_bottles.py

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,19 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+    i = num
+    x = "bottles"
+    y = "bottle"
 
+    while(i>0):
+        if(i != 1):
+            print(i, x, "of beer on the wall,", i, x, "of beer.")
+            print("Take one down and pass it around,", i-1, x, "of beer on the wall.")
+        else:
+            print(i, y, "of beer on the wall,", i, y, "of beer.")
+            print("Take one down and pass it around, no more bottles of beer on the wall.")
+            print("No more bottles of beer on the wall, no more bottles of beer.")
+            print("Go to the store and buy some more,", num, "bottles of beer on the wall.")
 
-bottle_song()
+        i -= 1
+
+# Main
+bottle_song(99)
